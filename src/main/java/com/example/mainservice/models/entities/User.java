a9adb71fb5d08aef27dd11c1018c6193640c2953
Sh1bari <vova_krasnov_2004@mail.ru>
2024-04-27 16:59:58 +0300

    done

---
+++ src/main/java/com/example/mainservice/models/entities/User.java
@@ -0,0 +1,37 @@
+package com.example.mainservice.models.entities;
+
+import jakarta.persistence.*;
+import lombok.*;
+import org.hibernate.annotations.UuidGenerator;
+
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.UUID;
+
+@Entity
+@Getter
+@Setter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "users")
+public class User {
+    @Id
+    @UuidGenerator
+    @Column(name = "id", nullable = false, updatable = false)
+    private UUID id;
+
+    private UUID authid;
+
+    @Column(unique = true)
+    private String username;
+
+    private String name;
+    private String middleName;
+    private String surname;
+    @Basic
+    private LocalDateTime registrationTime;
+
+
+}
